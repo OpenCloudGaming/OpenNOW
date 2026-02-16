09567abebbab27a0055cd6f2b61cd853e6dd86e6
Zortos <zortosdev@proton.me>
2026-02-16 21:25:25 +0100

    Del: Junk

--- _read4.js
+++
@@ -1,3 +0,0 @@
-const fs=require('fs'); 
-const l=fs.readFileSync('opennow-stable/src/renderer/src/gfn/inputProtocol.ts','utf8').split(/\r?\n/); 
-for(let i=300;i<380;i++)console.log((i+1)+': '+l[i]); 
